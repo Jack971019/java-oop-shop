fd0c73ae6c1f92a97f6ea56c1cd6ed242cdb5e64
Giacomotestori <giacomo.testori10@gmail.com>
2023-07-20 13:56:33 +0200

    class Product completed- main class started

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,4 +1,24 @@
 package org.lessons.java.shop;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("inserisci il nome del prodotto");
+        String name = scan.nextLine();
+
+        System.out.println("inserisci la descrizione del prodotto");
+        String description = scan.nextLine();
+
+        System.out.println("inserisci il prezzo del prodotto");
+        double price = Integer.parseInt(scan.nextLine());
+
+        System.out.println("inserisci il valore dell'iva sul prodotto");
+        double iva = Integer.parseInt(scan.nextLine());
+
+
+
+    }
 }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,4 +1,86 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Product {
+
+    // CAMPI
+
+    private int code;
+
+    private String name;
+
+    private String description;
+
+    private double price;
+
+    private double iva;
+
+    // COSTRUTTORI
+
+    public Product(int code){
+        this.code= new Random().nextInt(1,100000000);
+    }
+
+    public Product(String name, String description, double price, double iva){
+
+        this.name=name;
+        this.description= description;
+        this.price= price;
+        this.iva=iva;
+    }
+
+    // GETTER AND SETTER
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getIva() {
+        return iva;
+    }
+
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+
+
+
+    // METODI
+
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", iva=" + iva +
+                '}';
+    }
 }
